3bddf11d32160e07e20f5742cfc0271848336db9
Kazumi Inada <timer.ground.gm@gmail.com>
2019-03-08 23:57:13 +0900

    Set template to write out <title> tag

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
 
+add_theme_support( 'title-tag' );
 add_theme_support( 'post-thumbnails' );
 // wp_enqueue_script( 'global', get_template_directory_uri() . '/dist/app/global.js', true);
 add_action( 'wp_enqueue_scripts', function() {
