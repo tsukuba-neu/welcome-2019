a1c42b4e7d1875da233dda11d79cc726b089edef
Kazumi Inada <timer.ground.gm@gmail.com>
2019-03-08 23:48:37 +0900

    Add base of Home

---
+++ page-home.php
@@ -0,0 +1,100 @@
+<?php
+
+add_action( 'wp_enqueue_scripts', function() {
+  wp_enqueue_script( 'index', get_template_directory_uri() . '/dist/app/page-home.js', null, null, true );
+});
+
+get_header();
+
+?>
+
+<section class="eye-catch" id="eyecatch">
+  <?php echo get_the_post_thumbnail() ?>
+</section>
+
+
+
+<section class="category category--events">
+  <h2 class="category__title">新歓情報</h2>
+<?php
+
+$posts = get_posts( array(
+  'posts_per_page' => 4,
+  'orderby' => 'date',
+  'order' => 'DESC',
+  'category_name' => 'events',
+) );
+
+if( $posts ): ?>
+  <ul class="post-list category__post-list">
+<?php
+  foreach ( $posts as $post ):
+    setup_postdata( $post );
+    include( 'components/post-list-item.php' );
+  endforeach;
+?>
+  </ul>
+<?php else: ?>
+  <p class="category__no-content">コンテンツは順次公開中！</p>
+<?php endif; ?>
+</section>
+
+
+<section class="category category--about">
+  <h2 class="category__title">人形劇団NEUについて</h2>
+<?php
+
+$posts = get_posts( array(
+  'posts_per_page' => 4,
+  'orderby' => 'date',
+  'order' => 'DESC',
+  'category_name' => 'about',
+) );
+
+if( $posts ): ?>
+  <ul class="post-list category__post-list">
+<?php
+  foreach ( $posts as $post ):
+    setup_postdata( $post );
+    include( 'components/post-list-item.php' );
+  endforeach;
+?>
+  </ul>
+<?php else: ?>
+  <p class="category__no-content">コンテンツは順次公開中！</p>
+<?php endif; ?>
+</section>
+
+
+<section class="category category--culture">
+  <h2 class="category__title">もっと知りたい！人形劇の文化</h2>
+<?php
+
+$posts = get_posts( array(
+  'posts_per_page' => 4,
+  'orderby' => 'date',
+  'order' => 'DESC',
+  'category_name' => 'culture',
+) );
+
+if( $posts ): ?>
+  <ul class="post-list category__post-list">
+<?php
+  foreach ( $posts as $post ):
+    setup_postdata( $post );
+    include( 'components/post-list-item.php' );
+  endforeach;
+?>
+  </ul>
+<?php else: ?>
+  <p class="category__no-content">コンテンツは順次公開中！</p>
+<?php endif; ?>
+</section>
+
+
+
+
+<?php
+get_footer();
+
+?>
