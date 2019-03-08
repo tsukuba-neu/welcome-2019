6b9d632bf8f3398e4fa51bd6912d4b13c843d29f
Kazumi Inada <timer.ground.gm@gmail.com>
2019-03-08 22:42:23 +0900

    Fix typo

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support( 'post-thumbnails' );
 add_action( 'wp_enqueue_scripts', function() {
 
   wp_enqueue_script( 'my_script',
-  get_stylesheet_directory_uri().'/dist/app/global.js',
+  get_template_directory_uri().'/dist/app/global.js',
   null, null, true);
 
 });
