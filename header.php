1d6e7f7555a2ec0e772684bc7ae63132da6590c6
Kazumi Inada <timer.ground.gm@gmail.com>
2019-03-12 07:52:41 +0900

    Add favicon

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
+<?php
+  $dir = get_template_directory_uri();
+?>
+
 <!DOCTYPE html>
 <html>
   <head>
     <meta name="viewport" content="width=device-width" />
+    <link rel=”icon” href="<?php echo $dir . '/assets/icons/favicon.ico' ?>" />
+    <link rel=”icon” type="image/png" sizes="16x16" href="<?php echo $dir . '/assets/icons/favicon-16x16.png' ?>" />
+    <link rel=”icon” type="image/png" sizes="32x32" href="<?php echo $dir . '/assets/icons/favicon-32x32.png' ?>" />
     <?php wp_head(); ?>
   </head>
   <body <?php body_class(); ?>>
