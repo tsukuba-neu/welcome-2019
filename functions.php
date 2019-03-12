dd01d80be06ed7b7fbb52f721c4327ad852d9080
Kazumi Inada <timer.ground.gm@gmail.com>
2019-03-12 12:14:38 +0900

    Fix wrong name of script import

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ add_theme_support( 'post-thumbnails' );
 // wp_enqueue_script( 'global', get_template_directory_uri() . '/dist/app/global.js', true);
 add_action( 'wp_enqueue_scripts', function() {
 
-  wp_enqueue_script( 'my_script',
+  wp_enqueue_script( 'global',
   get_template_directory_uri().'/dist/app/global.js',
   null, null, true);
 
